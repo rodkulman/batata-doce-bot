3511ec0f0d2649ca96e43f07051a3a1c9bcaf47e
rodkulman <rodkulman@gmail.com>
2018-03-09 15:39:33 -0300

    IT LIVES

---
+++ src/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Rodkulman.Telegram
+{
+    public class Program
+    {
+        public static readonly TelegramBotClient Bot = new TelegramBotClient("564117884:AAGwBXuL3v6AteHstPJ6_N3XdmWbN_BhBz8");
+
+        public static void Main(string[] args)
+        {
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnInlineQuery += BotOnInlineQueryReceived;
+            Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Bot.StartReceiving();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.Text) { return; }
+
+            switch (message.Text.Split(' ').First())
+            {
+                case "/whatis":
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+                    
+                    foreach (var reply in WhatIsCommand.ReplyMessage(message.Text))
+                    {
+                        await Bot.SendTextMessageAsync(message.Chat.Id, reply, replyMarkup: new ReplyKeyboardRemove());
+                        await Task.Delay(1000);
+                    } 
+                    break;
+                default:
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "O que tu tentou fazer não é dank o suficiente", replyMarkup: new ReplyKeyboardRemove());
+                    break;
+            }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            await Bot.AnswerCallbackQueryAsync(
+                callbackQueryEventArgs.CallbackQuery.Id,
+                $"Received {callbackQueryEventArgs.CallbackQuery.Data}");
+        }
+
+        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+        {
+            Console.WriteLine($"Received inline query from: {inlineQueryEventArgs.InlineQuery.From.Id}");
+
+            InlineQueryResultBase[] results = {
+                new InlineQueryResultLocation(
+                    id: "1",
+                    latitude: 40.7058316f,
+                    longitude: -74.2581888f,
+                    title: "New York")   // displayed result
+                    {
+                        InputMessageContent = new InputLocationMessageContent(
+                            latitude: 40.7058316f,
+                            longitude: -74.2581888f)    // message if result is selected
+                    },
+
+                new InlineQueryResultLocation(
+                    id: "2",
+                    latitude: 13.1449577f,
+                    longitude: 52.507629f,
+                    title: "Berlin") // displayed result
+                    {
+
+                        InputMessageContent = new InputLocationMessageContent(
+                            latitude: 13.1449577f,
+                            longitude: 52.507629f)   // message if result is selected
+                    }
+            };
+
+            await Bot.AnswerInlineQueryAsync(
+                inlineQueryEventArgs.InlineQuery.Id,
+                results,
+                isPersonal: true,
+                cacheTime: 0);
+        }
+
+        private static void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+        {
+            Console.WriteLine($"Received inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
+        }
+    }
+}
---
+++ src/WhatIsCommand.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rodkulman.Telegram
+{
+    public static class WhatIsCommand
+    {
+        public static IEnumerable<string> ReplyMessage(string message)
+        {
+            var tokens = message.Split(' ');
+
+            if (tokens.Length == 1)
+            {
+                yield return "Pergunta alguma coisa né fio";
+                yield break;
+            }
+
+            if (tokens.Length == 2)
+            {
+                switch (tokens[1].ToLower())
+                {
+                    case "love":
+                        yield return "baby don't hurt me";
+                        yield return "don't hurt me";
+                        yield return "no more";
+                        yield break;
+                    case "life":
+                        yield return "42";
+                        yield break;
+                    case "man":
+                        yield return "a miserable pile of secrets";
+                        yield break;
+                    default:
+                        yield return "te acalma fdp falta mais ifs nesse código";
+                        yield break;
+                }
+            }
+
+            yield return "não vale perguntar mais que duas coisas.";
+        }
+    }
+}
