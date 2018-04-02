5ef97cf71195763b551acce7f0d5c0390d25b959
rodkulman <rodkulman@gmail.com>
2018-04-02 12:16:37 -0300

    Trazer uma pagina inteira do reddit e pegar posts de imagem.

--- src/reddit.cs
+++ src/reddit.cs
@@ -8,6 +8,7 @@ using System.Net;
 using Newtonsoft.Json.Linq;
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types.Enums;
+using System.Linq;
 
 namespace Rodkulman.Telegram
 {
@@ -43,9 +44,54 @@ namespace Rodkulman.Telegram
             }
         }
 
+        public static async Task GetRandomImage(Message message, string redditName)
+        {
+            var j = await GetSubReddit(redditName, "hot", null);
+
+            if (j["data"].Value<int>("dist") == 0) { return; }
+
+            if (j["data"]["children"].Where(x => x["data"].Value<string>("post_hint") == "image" && isDirectImageLink(x["data"].Value<string>("url"))).GetRandomElement() is JObject post)
+            {
+                var request = WebRequest.CreateHttp(post["data"].Value<string>("url"));
+
+                using (var response = await request.GetResponseAsync())
+                {
+                    await Program.Bot.SendPhotoAsync(message.Chat.Id, response.GetResponseStream(), post["data"].Value<string>("title"));
+                }
+            }
+
+            bool isDirectImageLink(string link)
+            {
+                return
+                    link.EndsWith("png", StringComparison.OrdinalIgnoreCase) ||
+                    link.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                    link.EndsWith("jpeg", StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
         private static async Task<JObject> GetRedditTop3(string redditName)
         {
             var request = WebRequest.CreateHttp($"https://www.reddit.com/r/{redditName}/top/.json?t=all&limit=3");
+
+            using (var response = await request.GetResponseAsync())
+            using (var stream = response.GetResponseStream())
+            using (var textReader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(textReader))
+            {
+                return await JObject.LoadAsync(jsonReader);
+            }
+        }
+
+        private static async Task<JObject> GetSubReddit(string redditName, string sort, string t)
+        {
+            var url = $"https://www.reddit.com/r/{redditName}/{sort}.json?sort={sort}";
+
+            if (sort == "controversial" || sort == "top")
+            {
+                url += $"&t={t}";
+            }
+
+            var request = WebRequest.CreateHttp(url);
 
             using (var response = await request.GetResponseAsync())
             using (var stream = response.GetResponseStream())
