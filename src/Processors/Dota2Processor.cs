f2b63d48d2b4bf7a7946229d22c8e1fcbbe49efc
Rodrigo Cullmann <rodkulman@gmail.com>
2019-08-31 20:16:38 -0300

    All dota responses up

--- src/Processors/Dota2Processor.cs
+++ src/Processors/Dota2Processor.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Newtonsoft.Json;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -10,30 +11,14 @@ namespace Rodkulman.Telegram
 {
     public class Dota2Processor
     {
-        class DialogResponse
-        {
-            public string Name { get; set; }
-            public AudioLink[] Links { get; set; }
-        }
-
-        class AudioLink
-        {
-            public string Link { get; set; }
-            public string Author { get; set; }
-        }
-
-        private DialogResponse[] responses;
+        private XDocument allResponses;
 
         public Dota2Processor()
         {
-            var serializer = JsonSerializer.CreateDefault();
-
-            using (var stream = System.IO.File.OpenRead("db/dota2-responses.json"))
-            using (var textReader = new StreamReader(stream))
-            using (var jsonReader = new JsonTextReader(textReader))
+            using (var stream = Resources.GetFile("resources/dota2-dialogs.xml"))
             {
-                responses = serializer.Deserialize<DialogResponse[]>(jsonReader);
-            }
+                allResponses = XDocument.Load(stream);
+            }            
         }
 
         /// <summary>
@@ -41,7 +26,7 @@ namespace Rodkulman.Telegram
         /// </summary>
         public bool IsDota2Reponse(Message message)
         {
-            return responses.Any(x => x.Name == message.Text);
+            return allResponses.Root.Elements("Dialog").Any(x => x.Attribute("Name").Value == message.Text);
         }
 
         /// <summary>
@@ -49,18 +34,18 @@ namespace Rodkulman.Telegram
         /// </summary>
         public async Task SendDotaResponse(Message message, string audioName)
         {
-            var dialogResponse = responses.FirstOrDefault(x => x.Name == message.Text);
+            var dialogResponse = allResponses.Root.Elements("Dialog").FirstOrDefault(x => x.Attribute("Name").Value == audioName);
 
             if (dialogResponse != null)
             {
                 await Program.Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadAudio);
 
-                var audioLink = dialogResponse.Links.GetRandomElement();
-                var request = WebRequest.CreateHttp(audioLink.Link);
+                var audioLink = dialogResponse.Elements("Link").GetRandomElement();
+                var request = WebRequest.CreateHttp(audioLink.Value);
 
                 using (var response = await request.GetResponseAsync())
                 {
-                    await Program.Bot.SendAudioAsync(message.Chat.Id, response.GetResponseStream(), title: audioName, performer: audioLink.Author, replyToMessageId: message.Chat.Type == ChatType.Group ? message.MessageId : 0);
+                    await Program.Bot.SendAudioAsync(message.Chat.Id, response.GetResponseStream(), title: audioName, performer: audioLink.Attribute("Author").Value, replyToMessageId: message.Chat.Type == ChatType.Group ? message.MessageId : 0);
                 }
             }
         }
